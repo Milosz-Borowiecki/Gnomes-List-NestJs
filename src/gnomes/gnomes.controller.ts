a8515b448c110ee115fd94636103e83c4cb837e8
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2022-02-24 00:01:56 +0100

    Setup controller and create dtos

--- src/gnomes/gnomes.controller.ts
+++ src/gnomes/gnomes.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import { CreateGnomeDto } from "./dtos/create-gnome.dto";
+import { EditGnomeDto } from "./dtos/edit-gnome.dto";
+import { GnomesService } from "./gnomes.service";
 
 @Controller('gnomes')
-export class GnomesController {}
+export class GnomesController {
+
+    constructor(private gnomesService: GnomesService){
+    }
+
+    @Get('')
+    getGnomes(){
+        
+    }
+
+    @Get('/:id')
+    getGnome(@Param('id') id: string) {
+        
+    }
+
+    @Post()
+    addGnome(@Body() body: CreateGnomeDto) {
+        
+    }
+
+    @Delete('/:id')
+    @HttpCode(204)
+    removeGnome(@Param('id') id: string){
+        
+    }
+
+    @Patch('/:id')
+    editGnome(@Body() body: EditGnomeDto, @Param('id') id: string) {
+        
+    }
+
+}
