9b5ae5fda746e5d4c9c42f44d9a5a4a3aaa2a0fa
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2022-02-24 00:58:36 +0100

    Validation setup

--- src/gnomes/dtos/create-gnome.dto.ts
+++ src/gnomes/dtos/create-gnome.dto.ts
@@ -1,1 +1,19 @@
-export class CreateGnomeDto {}
+import { IsNumber, IsString, Max, Min } from "class-validator";
+
+export class CreateGnomeDto {
+
+    @IsString()
+    name: string;
+
+    @IsNumber()
+    age: number;
+
+    @IsNumber()
+    @Min(0)
+    @Max(150)
+    strength: number;
+
+    @IsString()
+    race: string;
+
+}
--- src/gnomes/dtos/edit-gnome.dto.ts
+++ src/gnomes/dtos/edit-gnome.dto.ts
@@ -1,1 +1,21 @@
-export class EditGnomeDto {}
+import { IsNumber, IsString, Max, Min } from "class-validator";
+
+export class EditGnomeDto {
+
+    @IsString()
+    name: string;
+
+    @IsNumber()
+    @Min(0)
+    @Max(100)
+    age: number;
+
+    @IsNumber()
+    @Min(0)
+    @Max(150)
+    strength: number;
+
+    @IsString()
+    race: string;
+
+}
