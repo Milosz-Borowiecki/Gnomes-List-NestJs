fa740ac18a2c2ffb20fbfd06215e782b814fc4fa
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2022-02-27 04:32:29 +0100

    Setup modules and services for auth and user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GnomesModule } from './gnomes/gnomes.module';
+import { AuthModule } from './auth/auth.module';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -12,9 +15,11 @@ import { GnomesModule } from './gnomes/gnomes.module';
       database: 'db.sqlite',
       synchronize: true,
       entities: ['**/*.entity.js']
-    })
+    }),
+    AuthModule,
+    UserModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
