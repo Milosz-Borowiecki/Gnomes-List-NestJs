f02d01f7f32bd5a2aec53a39448dd3a635babd05
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2022-03-02 22:15:53 +0100

    Add functions for UserService and InjectRepository

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { randomBytes, scryptSync } from 'crypto';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+    async findOne(username: string): Promise<User | undefined>{
+        return this.repo.findOne({ username });
+    }
+
+    async create(username: string,password: string): Promise<any>{
+        const salt = randomBytes(16).toString('hex');
+        const hashedPassword = scryptSync(password,salt,64).toString('hex');
+
+        const newUser = this.repo.create({username,password:`${salt}:${hashedPassword}`});
+        this.repo.save(newUser);
+        return true;
+    }
+}
