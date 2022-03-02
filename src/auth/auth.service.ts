cbf75adeba7f3176730b1380c323bbc1eaef51b0
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2022-03-03 00:59:44 +0100

    Add validateUser method for AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { scryptSync, timingSafeEqual } from 'crypto';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UserService);
+    constructor(private userService: UserService){};
+
+    async validateUser(username: string,pass: string): Promise<any>{
+        const userValid = await this.userService.findOne(username);
+
+        const [salt, key] = userValid.password.split(':');
+        const hashedBuffer = scryptSync(pass,salt,64);
+
+        const keyBuffer = Buffer.from(key,'hex');
+        const match = timingSafeEqual(hashedBuffer,keyBuffer);
+
+        if (userValid && match) {
+            const { password, ...result } = userValid;
+            return result;
+        }
+        return null;
+    }
 }
