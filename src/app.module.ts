3e4c8dc9e21946e4794c78d65cc9f1d556958b02
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2022-03-04 00:15:09 +0100

    Update modules for app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GnomesModule } from './gnomes/gnomes.module';
 import { AuthModule } from './auth/auth.module';
-import { UserService } from './user/user.service';
-import { UserModule } from './user/user.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
-    GnomesModule,
     TypeOrmModule.forRoot({
       type:'sqlite',
       database: 'db.sqlite',
       synchronize: true,
       entities: ['**/*.entity.js']
     }),
+    GnomesModule,
     AuthModule,
-    UserModule
   ],
-  controllers: [AppController],
-  providers: [AppService, UserService],
+  providers:[AppService],
+  controllers:[AppController],
 })
 export class AppModule {}
