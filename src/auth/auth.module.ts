162096648378aa16701df820887353008c11ae52
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2022-03-03 18:16:24 +0100

    Update modules for auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserService } from 'src/user/user.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
   imports:[UserService],
-  providers: [AuthService],
-  controllers: [AuthController]
+  providers: [AuthService,PassportModule],
+  controllers: [AuthController,LocalStrategy]
 })
 export class AuthModule {}
